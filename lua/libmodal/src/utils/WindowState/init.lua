68e4537f15f6471791be087f2a64d9c011024033
Iron-E <code.iron.e@gmail.com>
2020-05-13 22:55:57 -0400

    Begin documentation

--- lua/libmodal/src/utils/WindowState/init.lua
+++ lua/libmodal/src/utils/WindowState/init.lua
@@ -23,21 +23,31 @@ local width = 'winwidth'
 	 */
 --]]
 
+--------------------------
+--[[ SUMMARY:
+	* Create a table representing the size of the current window.
+]]
+--[[ RETURNS:
+	* The new `WindowState`.
+]]
+--------------------------
 function WindowState.new()
 	local winState = {
 		['height'] = api.nvim_get_option(height),
 		['width'] = api.nvim_get_option(width),
 	}
 
+	---------------------------
+	--[[ SUMMARY
+		* Restore the state of `self`.
+	]]
+	---------------------------
 	function winState:restore()
 		api.nvim_set_option(height, self['height'])
 		api.nvim_set_option(width, self['width'])
 	end
 
 	return winState
-end
-
-function WindowState.restore(state)
 end
 
 --[[
