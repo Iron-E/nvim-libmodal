06648c277b7608f3f55eb079da550b0eb626c7ed
Iron-E <code.iron.e@gmail.com>
2021-07-07 12:40:52 -0400

    perf(plugin): simplify lua

--- plugin/libmodal.lua
+++ plugin/libmodal.lua
@@ -1,19 +1,13 @@
-local exe = vim.api.nvim_command
 local g = vim.g
-local go = vim.go
 
 if g.loaded_libmodal then return end
 g.loaded_libmodal = true
 
-if g.libmodalTimeouts == nil then
-	g.libmodalTimeouts = go.timeout
-end
-
---[[/* User Configuration */]]
+g.libmodalTimeouts = g.libmodalTimeouts or vim.go.timeout
 
 -- The default highlight groups (for colors) are specified below.
 -- Change these default colors by defining or linking the corresponding highlight group.
-exe
+vim.cmd
 [[
 	highlight default link LibmodalPrompt ModeMsg
 	highlight default link LibmodalStar StatusLine
